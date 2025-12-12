51b61fe1f68a3fbe262c44cb00709e1231640d74
paulsvc <paulsvc02@gmail.com>
2025-12-12 15:29:51 +0100

    up

--- lib/soes-esi/utypes.h
+++ lib/soes-esi/utypes.h
@@ -13,49 +13,39 @@ typedef struct
 
    /* Inputs */
 
-   float FrequencyCounterFrequency;
+   int32_t Input12;
    struct
    {
-      uint8_t IndexStatus;
-      uint8_t IndexByte;
-      float Position;
-      float Velocity;
-   } EncoderIn;
-   uint8_t Input8;
-   float ActualPosition1;
-   float ActualPosition2;
-   float ActualPosition3;
-   float ActualPosition4;
+      int32_t RawData;
+      float CalculatedVoltage;
+      uint32_t Status;
+      float LowpassFilteredVoltage;
+      uint8_t OhmicSensingSensed;
+   } Out_Unit1;
 
    /* Outputs */
 
+   uint8_t Output4;
    struct
    {
-      uint8_t IndexLatchEnable;
-      float Scale;
-   } EncoderOut;
-   uint8_t Output4;
-   uint8_t Enable1;
-   uint8_t Enable2;
-   uint8_t Enable3;
-   uint8_t Enable4;
-   float CommandedPosition1;
-   float CommandedPosition2;
-   float CommandedPosition3;
-   float CommandedPosition4;
-   float Voltage;
+      float VoltageScale;
+      float VoltageOffset;
+      float LowPassFilterThresholdVoltage;
+      uint8_t EnableOhmicSensing;
+      float OhmicSensingVoltageLimit;
+      float OhmicSensingVoltageDrop;
+   } In_Unit1;
 
    /* Parameters */
 
-   uint32_t BasePeriod;
-   float StepsPerMM1;
-   float StepsPerMM2;
-   float StepsPerMM3;
-   float StepsPerMM4;
-   float MaxAcceleration1;
-   float MaxAcceleration2;
-   float MaxAcceleration3;
-   float MaxAcceleration4;
+   struct
+   {
+      uint8_t I2C_devicetype;
+      uint8_t I2C_address;
+      uint32_t LowpassFilterPoleFrequency;
+      uint16_t OhmicSensingSetupTime;
+      uint16_t OhmicSensingPulseLength;
+   } Settings_Unit1;
 } _Objects;
 
 extern _Objects Obj;
